076872e45e6401c67f97a88e3ccc93ad51ed1056
Dongwon-tuna <61178312+Dongwon-tuna@users.noreply.github.com>
2022-12-30 00:01:12 +0900

    Add back button

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +10,7 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btn_move;
 
     EditText et_id;
     Button btn_test;
@@ -21,11 +23,25 @@ public class MainActivity extends AppCompatActivity {
         et_id = findViewById(R.id.et_id); //id에 숨을 불어 넣어 줌
         btn_test  = findViewById(R.id.btn_test);
 
+        btn_move = findViewById(R.id.btn_move);
+
+        // btn_test의 버튼 역할
         btn_test.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 et_id.setText("황동원1234");
             }
         });
+
+        // btn_move의 버튼 역할
+        btn_move.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,SubActivity.class);
+                startActivity(intent); // 액티비티 이동 해주는 구문
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/myapplication/SubActivity.java
+++ app/src/main/java/com/example/myapplication/SubActivity.java
@@ -2,13 +2,31 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SubActivity extends AppCompatActivity {
+
+
+    private Button btn_back ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
+
+        btn_back = findViewById(R.id.btn_back);
+
+        //-------------------------btn_back 기능
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SubActivity.this,MainActivity.class);
+                startActivity(intent); // 액티비티 이동 해주는 구문
+            }
+        });
+
     }
 }
