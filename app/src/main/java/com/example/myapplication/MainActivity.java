a015b446816c679c498584f4858cc65aa4855bf6
Dongwon-tuna <61178312+Dongwon-tuna@users.noreply.github.com>
2022-12-30 00:39:21 +0900

    Add intent

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,6 +11,8 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
 
     private Button btn_move;
+    private EditText et_test;
+    private String str ;
 
     EditText et_id;
     Button btn_test;
@@ -22,8 +24,10 @@ public class MainActivity extends AppCompatActivity {
 
         et_id = findViewById(R.id.et_id); //id에 숨을 불어 넣어 줌
         btn_test  = findViewById(R.id.btn_test);
-
+        //---------------------------페이지 이동 시 설정
         btn_move = findViewById(R.id.btn_move);
+        et_test = findViewById(R.id.et_test);
+
 
         // btn_test의 버튼 역할
         btn_test.setOnClickListener(new View.OnClickListener() {
@@ -37,7 +41,9 @@ public class MainActivity extends AppCompatActivity {
         btn_move.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                str = et_test.getText().toString();
                 Intent intent = new Intent(MainActivity.this,SubActivity.class);
+                intent.putExtra("str",str);
                 startActivity(intent); // 액티비티 이동 해주는 구문
             }
         });
--- app/src/main/java/com/example/myapplication/SubActivity.java
+++ app/src/main/java/com/example/myapplication/SubActivity.java
@@ -6,20 +6,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class SubActivity extends AppCompatActivity {
 
 
     private Button btn_back ;
+    private TextView tv_sub;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
 
+        Intent intent = getIntent();
+        String str = intent.getStringExtra("str");
+
+
+
+        tv_sub = findViewById(R.id.tv_sub);
+
+        tv_sub.setText(str);
+        //-------------------------btn_back 기능
         btn_back = findViewById(R.id.btn_back);
 
-        //-------------------------btn_back 기능
+
         btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
